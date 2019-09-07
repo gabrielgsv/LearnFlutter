b62d7d75c930b12f0d0e7fd9fbce13d6f202eece
gabrielgsv <gabriel.soares.vilela@gmail.com>
2019-09-07 16:17:17 -0300

    first commit

---
+++ contador_de_pessoas/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:contador_de_pessoas/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Home(),
+    );
+  }
+}
